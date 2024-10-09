8facdca7c5cad8d56660ea7772eeade0c09dc8c5
sardorevrika <bestevrika@gmail.com>
2024-10-09 16:44:27 +0500

    add files
    vazifa yuklandi

---
+++ 1-dars.py
@@ -0,0 +1,66 @@
+#                                              print()
+
+#print("Assalomu alaykum")
+
+#print(Hayrli tong)  # bu ishlamasligi sababi (" ") qo`shtirnoq yo`qligi, agar qo`shtirnoq qo`yilmasa matin emas raqam deb o`ylaydi
+
+#print('Men "Lenona" markasidagi nutbuk sotib oldim')
+
+#print("""Odami ersang,demagil odami,Oniki,yo`q xalq g`amidin g`ami """)
+
+#print('''Odami ersang,demagil odami,Oniki,yo`q xalq g`amidin g`ami ''')
+
+#print("Odami ersang,demagil odami,\nOniki,yo`q xalq g`amidin g`ami")
+
+#                                  SINTEKS XATOLIK (SYNTAX ERROR)
+
+
+
+#                                  ARIFMETIK AMALLAR
+
+#print(2+4*2)
+
+#print(20/5)
+
+#print(10//3)
+
+#print(2**4)
+
+#                                  ARIFMETIK AMALLAR VA MATIN
+
+#print("To`qqizning kvadrati",9**2,"ga teng")
+
+#print('3 * 3 = ',3*3) # Bu kod 3 sonining kvadratini hisoblaydi (comment)
+
+
+#                                 O`ZGARUVCHI (VARIABLE)
+
+#ism = "Jonibek"
+#yosh = 21
+
+#print(ism)
+#print(yosh)
+
+#ism = "Temurbek"
+#print(ism)
+
+#ism = "Javlonbek"
+#print(ism)
+
+#                                 O`ZGARUVCHILARNI NOMLASH
+
+#_ = "Sardorbek"
+#print(_)
+
+#Qwertyuiopasdfghjklzxcvbnm1234567890_ = "Sardorbek"
+#print(Qwertyuiopasdfghjklzxcvbnm1234567890_)
+
+ism = "sardor"
+print(ism)
+
+ISM = "SARDOR"
+print(ISM)
+
+Ism = "Sardor"
+print(Ism)
+
